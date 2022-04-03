9c568b462ebd7eed2c6317b0a0ee2a5ad4d97a56
Nicolas Casajus <nicolas.casajus@gmail.com>
2022-04-03 15:58:05 +0200

    Add makefile

---
+++ make.R
@@ -0,0 +1,29 @@
+#' Results of French Presidential Election
+#' 
+#' @description 
+#' __ADD DESCRIPTION__
+#' 
+#' @author Nicolas Casajus \email{nicolas.casajus@gmail.com}
+#' 
+#' @date 2022/04/03
+
+
+
+## Install dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load project addins (R functions and packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global variables ----
+
+year <- 2017
+
+
+## Run project ----
+
+# source(here::here("analyses", "script_X.R"))
